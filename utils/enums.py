0cf2f4995b1a90c432d2d7f60c7c67a5544d78f4
kishan <kishanvaghasiya3789@gmail.com>
2024-11-28 12:43:34 +0530

    add poetry

--- utils/enums.py
+++ utils/enums.py
@@ -4,3 +4,12 @@ from enum import Enum
 class ErrorMessageCodes(str, Enum):
     NOT_FOUND = "NOT_FOUND"
     BAD_REQUEST = "BAD_REQUEST"
+
+
+class Gender(int, Enum):
+    Male = "male"
+    Female = "female"
+
+
+class Currency(str, Enum):
+    INR = "₹"
