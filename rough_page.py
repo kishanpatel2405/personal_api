45e168e3b383da250821921760ec28f8d51217a5
kishan <kishanvaghasiya3789@gmail.com>
2024-12-26 12:27:11 +0530

    rough uses

---
+++ rough_page.py
@@ -0,0 +1,26 @@
+# def fibonacci_series(n):
+#     a, b = 0, 1
+#     series = []
+
+#     for _ in range(n):
+#         series.append(a)
+#         a, b = b, a + b
+
+#     return series
+
+# terms = int(input("Enter the number of terms: "))
+# print("Fibonacci series:", fibonacci_series(terms))
+
+num = int(input("Enter the number of terms: "))
+
+a = 0
+b = 1
+fibonacci_series = []
+
+for i in range(num):
+    fibonacci_series.append(a)
+    a, b = b, a + b
+
+
+print(fibonacci_series)
+
