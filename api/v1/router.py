44c2ad8d68762a1627c9223cfa101d09d32ba115
kishan <kishanvaghasiya3789@gmail.com>
2024-12-26 12:25:17 +0530

    change router file

--- api/v1/router.py
+++ api/v1/router.py
@@ -5,7 +5,7 @@ from api.v1.endpoints import (health,
                               crypto,
                               clean_temp,
                               weather,
-                              typing_test)
+                              )
 
 api_router = APIRouter()
 api_router.include_router(health.router, tags=["Health"])
@@ -16,4 +16,3 @@ api_router.include_router(crypto.router, prefix="/crypto", tags=["Crypto"])
 api_router.include_router(clean_temp.router, prefix="/clean-temp", tags=["System Maintenance"])
 
 api_router.include_router(weather.router, prefix="/weather", tags=["Weather"])
-api_router.include_router(typing_test.router, prefix="/typing-test", tags=["Typing Test"])
