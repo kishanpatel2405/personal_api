0a4b0ef0a64fc9c866bb36ad848579996c90c1e4
kishan <kishanvaghasiya3789@gmail.com>
2024-12-30 18:04:18 +0530

    add one more task for send email

--- celery_tasks/tasks.py
+++ celery_tasks/tasks.py
@@ -62,4 +62,38 @@ def forgot_password_mail(self, email: str, token: str):
         params={"name": name, "reset_password_link": token},
     )
     sib_api_v3_sdk.TransactionalEmailsApi(sendinblue_api_client).send_transac_email(send_smtp_email)
+@shared_task(
+    bind=True,
+    autoretry_for=(Exception,),
+    retry_backoff=True,
+    retry_kwargs={"max_retries": 5},
+)
+def send_credit_notification(
+    self,
+    first_name: str,
+    last_name: str,
+    company_name: str,
+    company_email: str,
+    subject: str,
+    previous_credit: str,
+    credit_change: str,
+    new_credit: str,
+    credit_type: str,
+):
+    send_smtp_email = sib_api_v3_sdk.SendSmtpEmail(
+        to=[{"name": f"{first_name} {last_name}", "email": company_email}],
+        subject=subject,
+        template_id=config.TEMPLATE_ID_OF_CREDIT_UPDATE_EMAIL_TEMPLATE,
+        params={
+            "first_name": first_name,
+            "last_name": last_name,
+            "company_name": company_name,
+            "subject": subject,
+            "previous_credit": previous_credit,
+            "credit_change": credit_change,
+            "new_credit": new_credit,
+            "credit_type": credit_type,
+        },
+    )
 
+    sib_api_v3_sdk.TransactionalEmailsApi(sendinblue_api_client).send_transac_email(send_smtp_email)
