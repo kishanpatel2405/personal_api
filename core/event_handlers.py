4e07a24b65ffd067d3c930926e4388e67e0d4d7e
kishan <kishanvaghasiya3789@gmail.com>
2024-12-31 17:16:54 +0530

    reformate code

--- core/event_handlers.py
+++ core/event_handlers.py
@@ -3,13 +3,14 @@ from typing import Callable
 
 import sib_api_v3_sdk
 from fastapi import FastAPI
+from pydantic import BaseSettings
 from sqlalchemy import URL, create_engine
 from sqlalchemy.orm import sessionmaker
-from pydantic import BaseSettings
 
 from utils.misc import TokenBackend
 
 logger = logging.getLogger(__name__)
+
 
 class Settings(BaseSettings):
     POSTGRES_USER: str
@@ -23,7 +24,9 @@ class Settings(BaseSettings):
     class Config:
         env_file = ".env"
 
+
 settings = Settings()
+
 
 async def _startup(app: FastAPI) -> None:
     configuration = sib_api_v3_sdk.Configuration()
@@ -37,16 +40,18 @@ async def _startup(app: FastAPI) -> None:
             port=5432,
             database="my_api",
         ).render_as_string(hide_password=False),
-            pool_size=settings.DB_POOL_SIZE,
-            max_overflow=settings.DB_MAX_OVERFLOW,
+        pool_size=settings.DB_POOL_SIZE,
+        max_overflow=settings.DB_MAX_OVERFLOW,
     )
     app.db_session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
     app.token_backend = TokenBackend("HS256", settings.SECRET_KEY, "", None, None, None, 0, None)
     app.sendinblue_api_client = sib_api_v3_sdk.ApiClient(configuration)
 
+
 def _shutdown(app: FastAPI) -> None:
     if hasattr(app, "db_session"):
         app.db_session.close_all()
+
 
 def start_app_handler(app: FastAPI) -> Callable:
     async def startup() -> None:
@@ -54,6 +59,7 @@ def start_app_handler(app: FastAPI) -> Callable:
         await _startup(app)
 
     return startup
+
 
 def stop_app_handler(app: FastAPI) -> Callable:
     def shutdown() -> None:
