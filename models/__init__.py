f90d8364780a47045caaa9ad1740bad4f62af101
kishan <kishanvaghasiya3789@gmail.com>
2025-01-01 10:37:11 +0530

    add code for user table

--- models/__init__.py
+++ models/__init__.py
@@ -41,3 +41,26 @@ class City(Base):
     state_id: Mapped[int] = mapped_column(Integer, ForeignKey("state_data.id"))
 
     state: Mapped["State"] = relationship("State", back_populates="cities")
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String, unique=True, index=True)
+    is_active: Mapped[str] = mapped_column(Boolean, default=True)
+    title: Mapped[str] = mapped_column(String, nullable=True)
+    first_name: Mapped[str] = mapped_column(String, nullable=True)
+    last_name: Mapped[str] = mapped_column(String, nullable=True)
+    gender: Mapped[str] = mapped_column(String, nullable=True)
+    cell_country_code: Mapped[str] = mapped_column(String, nullable=True)
+    mobile_number: Mapped[str] = mapped_column(String, nullable=True)
+    address: Mapped[str] = mapped_column(String, nullable=True)
+    country_id: Mapped[int] = mapped_column(Integer, ForeignKey("country_data.id"), nullable=True)
+    state_id: Mapped[int] = mapped_column(Integer, ForeignKey("state_data.id"), nullable=True)
+    city_id: Mapped[int] = mapped_column(Integer, ForeignKey("city_data.id"), nullable=True)
+    date_of_birth: Mapped[datetime] = mapped_column(DateTime, nullable=True)
+
+    country = relationship("Country", back_populates="users")
+    state = relationship("State", back_populates="users")
+    city = relationship("City", back_populates="users")
+
