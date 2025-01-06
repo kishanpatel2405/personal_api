8e450c6e23a60afcc58f4a9d772fbcaba784d715
kishan <kishanvaghasiya3789@gmail.com>
2025-01-06 23:10:41 +0530

    formate code

--- schemas/v1/health.py
+++ schemas/v1/health.py
@@ -1,6 +1,7 @@
-from typing import List, Dict
+from typing import Dict, List
 
 from pydantic import BaseModel, Field, condecimal
+
 from utils.enums import Ip_Type
 
 
@@ -10,6 +11,7 @@ class HealthResult(BaseModel):
     """
     is_alive: bool = Field(..., description="Indicates whether the system is alive or not.")
 
+
 class IPAddressResponse(BaseModel):
     """
     Response model for returning the IP address of the system.
@@ -17,12 +19,14 @@ class IPAddressResponse(BaseModel):
     ip_address: str = Field(..., description="The IP address of the system.")
     type: Ip_Type = Field(..., description="The type of IP address: LOCAL or EXTERNAL.")
 
+
 class SystemMetricsResponse(BaseModel):
     """
     Response model for returning system resource usage metrics.
     """
     cpu_usage: float = Field(..., ge=0, le=100, description="The CPU usage percentage of the system.")
     memory_usage: float = Field(..., ge=0, le=100, description="The memory usage percentage of the system.")
+
 
 class DiskUsageResponse(BaseModel):
     """
@@ -33,17 +37,20 @@ class DiskUsageResponse(BaseModel):
     free: int = Field(..., description="Free disk space in bytes.")
     percent: condecimal(ge=0, le=100) = Field(..., description="Disk usage percentage (0 to 100%).")
 
+
 class UptimeResponse(BaseModel):
     """
     Response model for returning the system uptime.
     """
     uptime: str = Field(..., description="System uptime in HH:MM:SS format.")
 
+
 class NetworkStatsResponse(BaseModel):
     """
     Response model for returning network statistics.
     """
     status: List[Dict[str, str]] = Field(..., description="List of network interface statistics.")
+
 
 class NetworkInterfaceStats(BaseModel):
     """
