9bffdebf8e33cc0c8bc8f1d06c91d33674ddea1c
kishan <kishanvaghasiya3789@gmail.com>
2025-01-08 23:34:51 +0530

    remove comments in schema of health

--- config.py
+++ config.py
@@ -11,15 +11,6 @@ class Config(BaseSettings):
     ENVIRONMENT: Environment
     API_PREFIX: str = "/api/"
     SECRET_KEY: str = secrets.token_urlsafe(32)
-    BACKEND_CORS_ORIGINS: List[str] = ["*"]
-
-    @field_validator("BACKEND_CORS_ORIGINS", mode="before")
-    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
-        if isinstance(v, str) and not v.startswith("["):
-            return [i.strip() for i in v.split(",")]
-        elif isinstance(v, (list, str)):
-            return v
-        raise ValueError(v)
 
     PROJECT_NAME: str
 
--- schemas/v1/health.py
+++ schemas/v1/health.py
@@ -6,32 +6,20 @@ from utils.enums import Ip_Type
 
 
 class HealthResult(BaseModel):
-    """
-    Response model indicating the health status of the system.
-    """
     is_alive: bool = Field(..., description="Indicates whether the system is alive or not.")
 
 
 class IPAddressResponse(BaseModel):
-    """
-    Response model for returning the IP address of the system.
-    """
     ip_address: str = Field(..., description="The IP address of the system.")
     type: Ip_Type = Field(..., description="The type of IP address: LOCAL or EXTERNAL.")
 
 
 class SystemMetricsResponse(BaseModel):
-    """
-    Response model for returning system resource usage metrics.
-    """
     cpu_usage: float = Field(..., ge=0, le=100, description="The CPU usage percentage of the system.")
     memory_usage: float = Field(..., ge=0, le=100, description="The memory usage percentage of the system.")
 
 
 class DiskUsageResponse(BaseModel):
-    """
-    Response model for returning disk usage statistics.
-    """
     total: int = Field(..., description="Total disk space in bytes.")
     used: int = Field(..., description="Used disk space in bytes.")
     free: int = Field(..., description="Free disk space in bytes.")
@@ -39,9 +27,6 @@ class DiskUsageResponse(BaseModel):
 
 
 class NetworkStatsResponse(BaseModel):
-    """
-    Response model for returning network statistics.
-    """
     status: List[Dict[str, str]] = Field(..., description="List of network interfaces with their statistics.")
 
 
