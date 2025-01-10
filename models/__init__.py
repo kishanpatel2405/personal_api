c357d5791e3bcbab21c96ea6f1aff1d1f3bac9b7
kishan <kishanvaghasiya3789@gmail.com>
2025-01-10 21:27:36 +0530

    remove some table

--- models/__init__.py
+++ models/__init__.py
@@ -7,49 +7,6 @@ from sqlalchemy.orm import (Mapped, declarative_base, mapped_column,
 Base = declarative_base()
 
 
-class Country(Base):
-    __tablename__ = "country_data"
-
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    name: Mapped[str] = mapped_column(String, index=True)
-    iso2: Mapped[str] = mapped_column(String, index=True)
-    iso3: Mapped[str] = mapped_column(String, index=True)
-    phone_code: Mapped[str] = mapped_column(String, index=True)
-    currency: Mapped[str] = mapped_column(String, index=True)
-    currency_name: Mapped[str] = mapped_column(String, index=True)
-    currency_symbol: Mapped[str] = mapped_column(String, index=True)
-    emoji: Mapped[str] = mapped_column(String, index=True)
-    emoji_iu: Mapped[str] = mapped_column(String, index=True)
-
-    states: Mapped[list["State"]] = relationship(
-        "State", back_populates="country", cascade="all, delete-orphan"
-    )
-
-
-class State(Base):
-    __tablename__ = "state_data"
-
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    name: Mapped[str] = mapped_column(String, index=True)
-    state_code: Mapped[str] = mapped_column(String, index=True)
-    country_id: Mapped[int] = mapped_column(Integer, ForeignKey("country_data.id"))
-
-    country: Mapped["Country"] = relationship("Country", back_populates="states")
-    cities: Mapped[list["City"]] = relationship(
-        "City", back_populates="state", cascade="all, delete-orphan"
-    )
-
-
-class City(Base):
-    __tablename__ = "city_data"
-
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    name: Mapped[str] = mapped_column(String, index=True)
-    state_id: Mapped[int] = mapped_column(Integer, ForeignKey("state_data.id"))
-
-    state: Mapped["State"] = relationship("State", back_populates="cities")
-
-
 class User(Base):
     __tablename__ = "users"
 
@@ -73,7 +30,10 @@ class User(Base):
         Integer, ForeignKey("city_data.id"), nullable=True
     )
     date_of_birth: Mapped[datetime] = mapped_column(DateTime, nullable=True)
+\
 
-    country = relationship("Country", back_populates="users")
-    state = relationship("State", back_populates="users")
-    city = relationship("City", back_populates="users")
+
+
+
+
+
